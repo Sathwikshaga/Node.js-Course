b03cc5f2671559386412864e0390a21ee41077b8
Sathwikshaga <shagasathwik@gmail.com>
2024-01-03 00:17:44 +0530

    server-sourceCodes

--- lesson-4/server.js
+++ lesson-4/server.js
@@ -6,14 +6,36 @@ const server = http.createServer((req, res) => {
 
     // set header content type
     res.setHeader("Content-Type", "text/html");
+    
+    let path = "./lesson-4/views/";
+    
+    switch (req.url) {
+        case "/":
+            path += "index.html";
+            res.statusCode = 200;
+            break;
+        case "/about":
+            path += "about.html";
+            res.statusCode = 200;
+            break;
+        case "/about-me":
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += "404.html";
+            res.statusCode = 404;
+            break;
+    }
 
-    fs.readFile(".lesson-4/views/index.html", (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err){
             console.log(err);
             res.end();
         }else{
-            res.write(data);
-            res.end();
+            // res.write(data);
+            res.end(data);
         }
     })
 });
