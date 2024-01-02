52912871f62da8e32098823250681f31bbee2c70
Sathwikshaga <shagasathwik@gmail.com>
2024-01-02 18:01:16 +0530

    create or delete files using node.js

--- lesson-2/files.js
+++ lesson-2/files.js
@@ -1,17 +1,50 @@
 const fs = require("fs");
 
 // reading files
-fs.readFile('./docs/blog1.txt', (err, data) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(data.tostring());
-});
+
+// fs.readFile('./docs/blog12.txt', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// });
 
 console.log("last line");
 
 // writing files
 
-// directories
+fs.writeFile("./docs/blog1.txt", "Hello, Sathwik", () => { // fs.writeFile => Asynchronous operations
+    console.log("file was written");
+});
+
+fs.writeFile("./docs/blog2.txt", "Hello, Again", () => {
+    console.log("file was written");
+});
+
+// Create and delete directories
+if (!fs.existsSync("./assets")) { // fs.existsSync, fs,mkdir, fs.rmdir => synchronous operations
+    fs.mkdir("./assets", (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("folder created");
+    });
+} else {
+    fs.rmdir("./assets", (err) => { // fs
+        if (err) {
+            console.log(err);
+        }
+        console.log("folder deleted");
+    });
+}
 
 // deleting files
+
+if(fs.existsSync("./docs/deleteMe.txt")){
+    fs.unlink("./docs/deleteMe.txt", (err) => {
+        if(err){
+            console.log(err);
+        }
+        console.log("file deleted");
+    })
+}
